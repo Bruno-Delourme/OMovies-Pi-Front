dfc830f7b4681f32989153edbc82d1f8b078857c
FadwaMAQUAIRE <fadwa.maquaire07@gmail.com>
2024-04-02 15:56:59 +0200

    update CSS KeywordBar

--- src/Components/KeywordBar/KeywordBar.tsx
+++ src/Components/KeywordBar/KeywordBar.tsx
@@ -1,8 +1,15 @@
 
+import "./KeywordBar.scss";
 
 function KeywordBar() {
     return (
-        <><h1>KeywordBar</h1><h2>Romance, Comédie, Marvel, science fiction, Animé </h2></>
+        <div className="div-keywordbar">
+            <button className="btn-romance">Romance</button>
+            <button className="btn-comédie">Comédie</button>
+            <button className="btn-marvel">Marvel</button>
+            <button className="btn-sciencefiction">science fiction</button>
+            <button className="btn-anime">Animé</button>
+        </div>
     );
 }
 
