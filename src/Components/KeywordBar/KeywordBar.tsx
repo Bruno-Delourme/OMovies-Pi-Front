38f58d7b43b8a1542166fbeda8def1820808184a
FadwaMAQUAIRE <fadwa.maquaire07@gmail.com>
2024-04-03 10:09:39 +0200

    last update CSS KeywordBar

--- src/Components/KeywordBar/KeywordBar.tsx
+++ src/Components/KeywordBar/KeywordBar.tsx
@@ -3,12 +3,12 @@ import "./KeywordBar.scss";
 
 function KeywordBar() {
     return (
-        <div className="div-keywordbar">
-            <button className="btn-romance">Romance</button>
-            <button className="btn-comédie">Comédie</button>
-            <button className="btn-marvel">Marvel</button>
-            <button className="btn-sciencefiction">science fiction</button>
-            <button className="btn-anime">Animé</button>
+        <div className="keywordbar">
+            <button className="keywordbar-btn" id="romance-btn">Romance</button>
+            <button className="keywordbar-btn" id="comedie-btn">Comédie</button>
+            <button className="keywordbar-btn" id="marvel-btn" >Marvel</button>
+            <button className="keywordbar-btn" id="sciencefiction-btn" >science fiction</button>
+            <button className="keywordbar-btn" id="anime-btn" >Animé</button>
         </div>
     );
 }
