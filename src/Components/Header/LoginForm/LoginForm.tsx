daea625c5df3a854d37d63e98e0cb8a6e86f12e1
Bruno-Delourme <delourmebruno@gmail.com>
2024-04-05 10:43:01 +0200

    login form com

--- src/Components/Header/LoginForm/LoginForm.tsx
+++ src/Components/Header/LoginForm/LoginForm.tsx
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from "react";
-import "./LoginForm.scss";
+/*import React, { useState, useEffect } from "react";
+import "./LoginForm.scss";*/
 
 function LoginForm () {
-  
+  /*
   const [showLoginForm, setShowLoginForm] = useState<boolean>(true); // on initialise à true au début, mais à modifier plus tard
 
   // Fonction pour basculer l'affichage du LoginForm 
   const toggleLoginForm = () => {
     setShowLoginForm(!showLoginForm); // Bascule l'état de visibilité
-  };
+  };*/
 
   return (
     <>
-      <div className="logo-container">
+      {/**<div className="logo-container">
         <button className="search-button" onClick={toggleLoginForm}>
-          <img src={logoAvatar} alt="Login/Subscribe" /> {/* logoAvatar défa défini*/}
+          <img src={logoAvatar} alt="Login/Subscribe" /> {/* logoAvatar défa défini}
         </button>
       </div>
       {showLoginForm && ( // Affiche le LoginForm si showLoginForm est true 
         <div className="LoginForm active"></div>
-      )}
+      )} */}
     </>
   );
 }
