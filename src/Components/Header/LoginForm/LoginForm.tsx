ced9081b6f1c41c8abff7e93ca53b0f0037a66cd
Bruno-Delourme <delourmebruno@gmail.com>
2024-04-08 13:30:05 +0200

    loginForm subscribe complete

--- src/Components/Header/LoginForm/LoginForm.tsx
+++ src/Components/Header/LoginForm/LoginForm.tsx
@@ -4,9 +4,46 @@ import "./LoginForm.scss";
 
 function LoginForm() {
   const [showLoginForm, setShowLoginForm] = useState(false);
+  const [pseudo, setPseudo] = useState("");
+  const [email, setEmail] = useState("");
+  const [date_of_birth, setdate_of_birth] = useState("");
+  const [password, setPassword] = useState("");
 
   const toggleLoginForm = () => {
     setShowLoginForm(!showLoginForm);
+  };
+
+  const handleSubmit = async (event : any) => {
+    event.preventDefault(); // pour empêcher le rechargement de la page
+
+    // L'URL back
+    const url = "http://localhost:3000/api/user";
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          pseudo,
+          email,
+          date_of_birth,
+          password,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Erreur: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Réponse du serveur:', data);
+      // afin de traiter la réponse
+
+    } catch (error) {
+      console.error(`Erreur lors de l'envoi des données:`, error);
+    }
   };
 
   return (
@@ -17,20 +54,19 @@ function LoginForm() {
         </button>
       </div>
       
-        <div className={`LoginForm ${showLoginForm ? "active" : ""}`}>
-          {/* Bouton de fermeture */}
-          <button className="close-button" onClick={toggleLoginForm}>
-            <FaTimes size={20} />
-          </button>
-          <form className="inscription-form">
-            <input type="text" placeholder="Pseudo" />
-            <input type="email" placeholder="Email"/>
-            <input type="birth" placeholder="Date de naissance"/>
-            <input type="password" placeholder="Mot de Passe"/>
-            <button type="submit">S'inscrire</button>
-          </form>
-        </div>
-      
+      <div className={`LoginForm ${showLoginForm ? "active" : ""}`}>
+        <button className="close-button" onClick={toggleLoginForm}>
+          <FaTimes size={20} />
+        </button>
+        <form className="inscription-form" onSubmit={handleSubmit}>
+          <input type="text" placeholder="Pseudo" value={pseudo} onChange={(e) => setPseudo(e.target.value)} />
+          <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="date" placeholder="Date de naissance" value={date_of_birth} onChange={(e) => setdate_of_birth(e.target.value)} />
+          <input type="password" placeholder="Mot de Passe" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button type="submit">S'inscrire</button>
+          
+        </form>
+      </div>
     </>
   );
 }
