31fa16801c4c28e36e2fe7a4a5413ac9bd0a21a0
Bruno-Delourme <delourmebruno@gmail.com>
2024-04-10 17:00:04 +0200

    modif types

--- src/@types/movie.d.ts
+++ src/@types/movie.d.ts
@@ -1,7 +1,7 @@
 export interface Movie {
     vote_average: number;
     id: number;
-    genre_ids: number;
+    
     adult: boolean;
     title: string;
     original_title: string;
@@ -10,6 +10,11 @@ export interface Movie {
     release_date: string;
     original_language: string;
     key: number;
+    //casting
+    cast_id:              number;
+    character:            string;
+    name:                 string;
+    genre_ids: number;
   }
   
   export interface MoviesResponse {
@@ -19,16 +24,3 @@ export interface Movie {
     total_results: number;
   }
 
-  export interface Cast {
-    gender:               number;
-    cast_id:              number;
-    known_for_department: string;
-    name:                 string;
-    original_name:        string;
-    popularity:           number;
-    profile_path:         string;
-    cast_id:              number;
-    character:            string;
-    credit_id:            string;
-    order:                number;
-  }
