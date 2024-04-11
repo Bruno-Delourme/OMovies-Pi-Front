cc31393d14eb5b78ef70eccae3c763f01728772f
Bruno-Delourme <delourmebruno@gmail.com>
2024-04-11 19:17:34 +0200

    details for one movie and buttons

--- src/Components/OneMovie/OneMovie.tsx
+++ src/Components/OneMovie/OneMovie.tsx
@@ -1,33 +1,32 @@
+import React from 'react';
 import { Movie } from "../../../src/@types/movie";
-import React, { useState, useEffect } from 'react';
+import '../../index.css'; 
 
-
-function OneMovie({ id, title, poster_path, overview, name,
-  genre_ids, release_date, vote_average }: Partial<Movie>) { // utilisation de Partial parce qu'on utilise pas tout les type de l'interface Movie, par exemple le cast n'est pas encore ajouté
-
-    const [isDetailsVisible, setIsDetailsVisible] = useState(false);
-
-  // Fonction pour basculer la visibilité
-  const toggleDetails = () => {
-    setIsDetailsVisible(!isDetailsVisible);
-  };
-
-  return (
-    
-    <div className="movie-details" onClick={toggleDetails}> {/* Ajout du gestionnaire de clic */}
-      {poster_path && <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={title} className="max-w-xs"/>}
-      
-      {isDetailsVisible && (
-        <div>
-          <h2>{title}</h2>
-          <p>Release Date: {release_date}</p>
-          <p>{genre_ids}</p>
-          <p>Cast : {name}</p>
-          <p>{overview}</p>
-          <p>Vote Average: {vote_average}</p> 
+function OneMovie({ id, title, poster_path, overview, name, genre_ids, release_date, vote_average }: Partial<Movie>) {
+    return (
+        <div className="group movie-details-container">
+            <div className="movie-details">
+                {poster_path && (
+                    <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={title} className="movie-poster"/>
+                )}
+                <div className="details group-hover:block hidden">
+                    <div className="details-background">
+                        <h2>{title}</h2>
+                        <p>Release Date: {release_date}</p>
+                        <p>{genre_ids}</p>
+                        <p>Cast: {name}</p>
+                        <p>{overview}</p>
+                        <p>Vote Average: {vote_average}</p>
+                    </div>
+                </div>
+            </div>
+            <div className="button-movieDetails opacity-0 group-hover:opacity-100 flex justify-around" style={{ transition: 'opacity .3s ease' }}>
+                <button><img src="/thumbs-up.svg" alt="Like" /></button>
+                <button><img src="/circle-plus.svg" alt="Add" /></button>
+                <button><img src="/presentation.svg" alt="Present" /></button>
+            </div>
         </div>
-      )}
-    </div>
-  );
+    );
 }
+
 export default OneMovie;
