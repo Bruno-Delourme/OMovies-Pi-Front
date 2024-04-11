b159daa0b0a48d5b17fda405e1af80cab268eb9b
Bruno-Delourme <delourmebruno@gmail.com>
2024-04-11 12:04:25 +0200

    OneMovie details on click but need to be changed on pointer

--- src/Components/OneMovie/OneMovie.tsx
+++ src/Components/OneMovie/OneMovie.tsx
@@ -1,20 +1,32 @@
-interface MovieProps {
-  id: number;
-  title: string;
-  poster_path?: string;
-  overview: string;
-  release_date: string;
-  vote_average: number;
-}
-function OneMovie({ id, title, poster_path, overview, release_date, vote_average }: MovieProps) {
+import { Movie } from "../../../src/@types/movie";
+import React, { useState, useEffect } from 'react';
+
+
+function OneMovie({ id, title, poster_path, overview, name,
+  genre_ids, release_date, vote_average }: Partial<Movie>) { // utilisation de Partial parce qu'on utilise pas tout les type de l'interface Movie, par exemple le cast n'est pas encore ajouté
+
+    const [isDetailsVisible, setIsDetailsVisible] = useState(false);
+
+  // Fonction pour basculer la visibilité
+  const toggleDetails = () => {
+    setIsDetailsVisible(!isDetailsVisible);
+  };
+
   return (
-    <div className="">
-    {/* Responsive Container vignette-movie w-1/5 sm:w-1/4 md:w-1/6 lg:w-1/8 xl:w-1/10 p-2*/}
-      {poster_path && <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={title} className="max-w-xs"/>} {/* show img only if poster_path exists */}
-      {/* <h2>{title}</h2>
-      <p>{overview}</p>
-      <p>Release Date: {release_date}</p>
-      <p>Vote Average: {vote_average}</p> */}
+    
+    <div className="movie-details" onClick={toggleDetails}> {/* Ajout du gestionnaire de clic */}
+      {poster_path && <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={title} className="max-w-xs"/>}
+      
+      {isDetailsVisible && (
+        <div>
+          <h2>{title}</h2>
+          <p>Release Date: {release_date}</p>
+          <p>{genre_ids}</p>
+          <p>Cast : {name}</p>
+          <p>{overview}</p>
+          <p>Vote Average: {vote_average}</p> 
+        </div>
+      )}
     </div>
   );
 }
