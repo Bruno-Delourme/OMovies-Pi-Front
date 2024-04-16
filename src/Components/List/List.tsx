6501d6de09693016f66db9bb136c997ed7342cfc
FadwaMAQUAIRE <fadwa.maquaire07@gmail.com>
2024-04-16 11:03:14 +0200

    update h1 profil and list

--- src/Components/List/List.tsx
+++ src/Components/List/List.tsx
@@ -1,9 +1,24 @@
 
+import { useAppDispatch, useAppSelector } from "../../hooks/redux";
 import HeaderList from "./HeaderList/HeaderList";
 
 function List() {
+
+    const dispatch = useAppDispatch();
+
+    const user = useAppSelector((state) => state.user);
+    
     return (
-        <><HeaderList /><h1>List</h1></>
+        <>
+            <HeaderList />
+            <h1>Bienvenue dans ton espace liste  {user.pseudo}</h1>
+
+            <label>
+                Filtrer par genre :
+                <input type="text" name="filterByGenre"  />
+            </label>
+
+        </>
     );
 }
 
