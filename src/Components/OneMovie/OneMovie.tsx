738a441e571390ad2097560c573e971507fbac30
Bruno <delourmebruno@gmail.com>
2024-04-16 05:59:12 +0000

    pbm with tailwind,onemovie withtou style

--- src/Components/OneMovie/OneMovie.tsx
+++ src/Components/OneMovie/OneMovie.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Movie } from "../../../src/@types/movie";
 import '../../index.css'; 
+import './OneMovie.scss';
 
-// function OneMovie({ id, title, poster_path, overview, name, genre_ids, release_date, vote_average }: Partial<Movie>) {
-//     const [animate, setAnimate] = React.useState(false);
+function OneMovie({ id, title, poster_path, overview, name, genre_ids, release_date, vote_average }: Partial<Movie>) {
+    const [animate, setAnimate] = React.useState(false);
 
-function OneMovie({ id, title, poster_path, overview, release_date, vote_average }: Movie) {
+//function to activate the animation on pointer    
+  const toggleAnimation = () => {
+      setAnimate(!animate);
+};
+
+//function to desactivate the animation on pointer
+  const handlePointerLeave = () => {
+    setAnimate(false);
+};
+
+
+const containerClass = animate ? "onemoviecontainer animate" : "onemoviecontainer";
+
   return (
-    <div className="onemoviecontainer"> 
-    {/* Responsive Container vignette-movie w-1/5 sm:w-1/4 md:w-1/6 lg:w-1/8 xl:w-1/10 p-2*/}
-      {poster_path && <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={title} className="max-w-xs rounded-md"/>} {/* show img only if poster_path exists */}
-      {/* <h2>{title}</h2>
+    <div className={containerClass} onPointerEnter={toggleAnimation} onPointerLeave={handlePointerLeave}> 
+    
+      {poster_path && <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={title} className="max-w-xs rounded-md"/>} 
+      <h2>{title}</h2>
       <p>{overview}</p>
       <p>Release Date: {release_date}</p>
-      <p>Vote Average: {vote_average}</p> */}
+      <p>Vote Average: {vote_average}</p> 
     </div>
   );
 }
