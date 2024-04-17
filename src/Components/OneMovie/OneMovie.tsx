f200e4a0ba777fe0753dfecd4eac1803868730b2
Bruno <delourmebruno@gmail.com>
2024-04-17 09:25:26 +0000

    try to send oneMovie to backend

--- src/Components/OneMovie/OneMovie.tsx
+++ src/Components/OneMovie/OneMovie.tsx
@@ -1,57 +1,100 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import {  useAppSelector } from '../../hooks/redux';
+import { UserFormData } from '../../@types/user';
+import axios from "axios";
 import { Movie } from "../../../src/@types/movie";
-import '../../index.css'; 
-import './OneMovie.scss';
-import { Link } from 'react-router-dom';
-import { Button } from '@mui/material';
-const LogoPlus = '../../../public/circle-plus.svg';
-const LogoLike = '../../../public/thumbs-up.svg';
-const LogoPlateform = '../../../public/presentation.svg';
+import "../../index.css";
+import "./OneMovie.scss";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
+const LogoPlus = "../../../public/circle-plus.svg";
+const LogoLike = "../../../public/thumbs-up.svg";
+const LogoPlateform = "../../../public/presentation.svg";
 
-function OneMovie({ id, title, poster_path, overview, name, genre_ids, release_date, vote_average }: Partial<Movie>) {
-    const [animate, setAnimate] = useState(false);
-    const [liked, setLiked] = useState(false);
+function OneMovie({
+  id,
+  title,
+  poster_path,
+  overview,
+  name,
+  genre_ids,
+  release_date,
+  vote_average,
+}: Partial<Movie>) {// utilise partiellement les propriéts de OneMovie pour éviter les bugs
+  const [animate, setAnimate] = useState(false);// État pour contrôler l'animation au survol de l'élément
+  const [liked, setLiked] = useState(false);// État pour contrôler si un film est liké
 
-    // Animation au survol
-    const toggleAnimation = () => {
-        setAnimate(true);
-    };
+  // Récupération de l'ID utilisateur depuis l'état Redux
+  const user = useAppSelector((state) => state.user);
 
-    // Desactivation de l'animation au survol
-    const handlePointerLeave = () => {
-        if (!liked) {  // Conserve l'animation si liké
-            setAnimate(false);
-        }
-    };
+  // Gère l'activation de l'animation lors du survol de la souris
+  const toggleAnimation = () => {
+    setAnimate(true);
+  };
 
-    // Statut toggle du like
-    const toggleLike = () => {
-        setLiked(!liked);
-        setAnimate(true);  // Conserve le bouton vert si liké
-    };
+  // Gère la désactivation de l'animation lorsque la souris est hors de l'élément
+  const handlePointerLeave = () => {
+    if (!liked) {// Si le film n'est pas liké, désactive l'animation
+      // Conserve l'animation si liké
+      setAnimate(false);
+    }
+  };
 
-    const containerClass = animate || liked ? "onemoviecontainer animate" : "onemoviecontainer";  // Conserve le bouton vert si liké
-    const likeButtonClass = liked ? "like-button liked" : "like-button";
+  // Gère le clic sur le bouton like, alternant l'état liké et déclenchant les actions
+  const toggleLike = async () => {
+    const newLikedStatus = !liked;
+    setLiked(newLikedStatus);
+    setAnimate(true);
 
-    return (
-        <div className={containerClass} onPointerEnter={toggleAnimation} onPointerLeave={handlePointerLeave}> 
-        {poster_path && <Link to={`/movie/${id}`}>
-            <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={title} className="movie-poster max-w-xs rounded-md" />
-          </Link>}
-          {/*<h2 className="movie-title">{title}</h2>NE PASSE PAS*/}
-          <div className="action-buttons">
-            <Button className={likeButtonClass} onClick={toggleLike}>
-              <img src={LogoLike} alt="Like" />
-            </Button>
-            <Button className="plus-button">
-              <img src={LogoPlus} alt="Plus" />
-            </Button>
-            <Button className="platform-button">
-              <img src={LogoPlateform} alt="Platform" />
-            </Button>
-          </div>
-        </div>
-    );
+    if (newLikedStatus) { // Effectue l'action uniquement si le film est liké
+      try {
+        const response = await axios.post(`/addToFavorite/${id}`, {
+          userId: user.id  // Utilise l'ID utilisateur récupéré depuis Redux
+        }, {
+          headers: {
+            Authorization: `Bearer ${user.token}`  // Supposons que l'authentification est nécessaire
+          }
+        });
+        console.log('Film added to favorites!');
+      } catch (error) {
+        console.error('Failed to add film to favorites:', error);
+      }
+    }
+  };
+
+  const containerClass = animate || liked ? "onemoviecontainer animate" : "onemoviecontainer";
+  const likeButtonClass = liked ? "like-button liked" : "like-button";
+
+  // Rendu du composant avec la logique conditionnelle pour afficher les boutons et les images
+  return (
+    <div
+      className={containerClass}
+      onPointerEnter={toggleAnimation}
+      onPointerLeave={handlePointerLeave}
+    >
+      {poster_path && (
+        <Link to={`/movie/${id}`}>
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+            alt={title}
+            className="movie-poster max-w-xs rounded-md"
+          />
+        </Link>
+      )}
+      {/*<h2 className="movie-title">{title}</h2>NE PASSE PAS*/}
+      <div className="action-buttons">
+        <Button className={likeButtonClass} onClick={toggleLike}>
+          <img src={LogoLike} alt="Like" />
+        </Button>
+        <Button className="plus-button">
+          <img src={LogoPlus} alt="Plus" />
+        </Button>
+        <Button className="platform-button">
+          <img src={LogoPlateform} alt="Platform" />
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default OneMovie;
